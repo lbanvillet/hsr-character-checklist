60496739b4d39af4a0cfc5198f0edcb7462362aa
Louis <louis.banvillet@gmail.com>
2024-01-31 11:43:54 +0000

    Attemp to make Puppeteer work

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const getCharacters = async () => {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: null,
+    executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-121.0.6167.85',
   });
 
   const page = await browser.newPage();
